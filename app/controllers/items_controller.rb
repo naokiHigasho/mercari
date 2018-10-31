2f39e7569674fae1b76ae005ac6e9df6dad20590
naokiHigashio <naokihigashio@gmail.com>
2018-10-31 14:18:34 +0900

    init

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,41 @@
+class ItemsController < ApplicationController
+  before_action :set_target_item, only: %i[show edit update destroy]
+
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to item
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    redirect_to @item
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit( )
+  end
+
+  def set_target_item
+    @item = Item.find(params[:id])
+  end
+
+end
